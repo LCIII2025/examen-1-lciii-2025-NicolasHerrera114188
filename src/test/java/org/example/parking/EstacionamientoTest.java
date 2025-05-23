b8d27ba49acbefacbc062f02ff2688dc7c06872f
114188_NicolasHerrera <114188@tecnicatura.frc.utn.edu.ar>
2025-05-22 21:14:52 -0300

    test en prueba

--- src/test/java/org/example/parking/EstacionamientoTest.java
+++ src/test/java/org/example/parking/EstacionamientoTest.java
@@ -1,20 +1,28 @@
 package org.example.parking;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.*;
 
 public class EstacionamientoTest {
 
     @Test
     public void testRetirarVehiculo() throws Exception {
         //TODO test
+
+        assertNotNull(true);
     }
 
     @Test
     public void testCalcularPrecio() throws Exception {
         // TODO test
+        Cliente cliente=new Cliente("123","nico");
+        Vehiculo vehiculo=new Vehiculo("78wer","vw", Vehiculo.Tipo.AUTO);
+        Ticket ticket=new Ticket(cliente,vehiculo);
+        double precio=ticket.calcularPrecio();
+        assertNotNull(precio);
+        
     }
 
 }
